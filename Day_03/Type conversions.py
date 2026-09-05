3079d9192b63ab90661f000459b631d378f2d2b2
Kopparapu Krishna Bala Sai <krishnabalasai123@gmail.com>
2026-09-05 14:50:51 +0530

    Data Types & Type Conversions

---
+++ Day_03/Type conversions.py
@@ -0,0 +1,132 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 10
+>>> float(a)
+10.0
+>>> str(a)
+'10'
+>>> complex(a)
+(10+0j)
+>>> list(a)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    list(a)
+TypeError: 'int' object is not iterable
+>>> tuple(a)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    tuple(a)
+TypeError: 'int' object is not iterable
+>>> set(a)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    set(a)
+TypeError: 'int' object is not iterable
+>>> dict(a)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    dict(a)
+TypeError: 'int' object is not iterable
+>>> bool(a)
+True
+>>> #float
+>>> int(a)
+10
+>>> complex(a)
+(10+0j)
+>>> str(a)
+'10'
+>>> list(a)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    list(a)
+TypeError: 'int' object is not iterable
+>>> tuple(a)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    tuple(a)
+TypeError: 'int' object is not iterable
+set(a)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    set(a)
+TypeError: 'int' object is not iterable
+dict(a)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    dict(a)
+TypeError: 'int' object is not iterable
+bool(a)
+True
+#complex
+int(a)
+10
+float(a)
+10.0
+str(a)
+'10'
+list
+<class 'list'>
+list(a)
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    list(a)
+TypeError: 'int' object is not iterable
+tuple(a)
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    tuple(a)
+TypeError: 'int' object is not iterable
+set(a)
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    set(a)
+TypeError: 'int' object is not iterable
+frozenset(a)
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    frozenset(a)
+TypeError: 'int' object is not iterable
+dict(a)
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    dict(a)
+TypeError: 'int' object is not iterable
+bool(a)
+True
+#String
+int(a)
+10
+float(a)
+10.0
+complex(a)
+(10+0j)
+str(a)
+'10'
+bool(a)
+True
+list(a)
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    list(a)
+TypeError: 'int' object is not iterable
+tuple(a)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    tuple(a)
+TypeError: 'int' object is not iterable
+set(a)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    set(a)
+TypeError: 'int' object is not iterable
+frozenset(a)
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    frozenset(a)
+TypeError: 'int' object is not iterable
+dict(a)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    dict(a)
+TypeError: 'int' object is not iterable
